47d5d25eff35bea092e63e056c7ec6f5713d7753
Arif <muhammadarifhermawan4@gmail.com>
2024-11-29 22:47:20 +0700

    feat: add player winning record

--- history.py
+++ history.py
@@ -1,11 +1,22 @@
 HISTORY = {}
+WINNING_HISTORY = {}
+
 
 def record(round: int, player: str, hand: str):
+    global HISTORY
     if round not in HISTORY:
         HISTORY[round] = {}
     if player not in HISTORY[round]:
         HISTORY[round][f"{player}"] = []
     HISTORY[round][player].append(hand.lower())
+
+def win_record(round: int, player: str, is_win: bool):
+    global WINNING_HISTORY
+    if round not in WINNING_HISTORY:
+        WINNING_HISTORY[round] = {}
+    if player not in WINNING_HISTORY[round]:
+        WINNING_HISTORY[round][f"{player}"] = []
+    WINNING_HISTORY[round][player].append(is_win)
 
 if __name__ == '__main__':
     record(1, 'p-1', 'stand')
